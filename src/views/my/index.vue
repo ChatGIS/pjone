d3aeecfdbcc023e15148eec5dbe995ebebd5b245
Dreamice <dreamice13@foxmail.com>
2024-04-08 23:16:02 +0800

    feat: 安装echarts；添加日历图展示

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -1,10 +1,52 @@
-<script setup lang='ts'>
+<!--
+ * @Author: Dreamice dreamice13@foxmail.com
+ * @Date: 2024-04-03 23:07:33
+ * @LastEditors: Dreamice dreamice13@foxmail.com
+ * @LastEditTime: 2024-04-10 12:41:07
+ * @FilePath: \pjone\src\views\my\index.vue
+ * @Description: 
+-->
+<script setup>
+import * as echarts from 'echarts'
+import { onMounted } from 'vue'
+import { getRecordsNum } from '@/api/life'
 
+
+onMounted(() => {
+  var chartDom = document.getElementById('main')
+  var myChart = echarts.init(chartDom)
+  var option
+
+  const initRecommendWebsites = async () => {
+    const res = await getRecordsNum()
+    console.log(res)
+    option = {
+      visualMap: {
+        show: false,
+        min: 0,
+        max: 10
+      },
+      calendar: {
+        range: '2024'
+      },
+      series: {
+        type: 'heatmap',
+        coordinateSystem: 'calendar',
+        data: res
+      }
+    }
+
+    option && myChart.setOption(option) 
+  }
+  // 初始化查询
+  initRecommendWebsites()  
+}
+)
 </script>
 
 <template>
     <div>
-        <h1>我的</h1>
+        <div id="main" style="width: 600px;height:400px;"></div>
     </div>
 </template>
 
