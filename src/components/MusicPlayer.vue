c820bb2b55d1147b6e2a5255fbc07dc492320d3e
ChatGIS <ChatGIS@outlook.com>
2024-11-27 23:54:23 +0800

    update music-player

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -5,9 +5,9 @@
             <div>
                 <span>{{ currentTimeFormatted }}</span>
             </div>
-            <el-button @click="prevSong" :icon="ArrowLeft" circle size="small"></el-button>
+            <el-button @click="changePreSong" :icon="ArrowLeft" circle size="small"></el-button>
             <el-button @click="togglePlay" :icon="isPlaying ? VideoPause : VideoPlay" circle></el-button>
-            <el-button @click="handleCurrentEnd" :icon="ArrowRight" circle size="small"></el-button>
+            <el-button @click="changeNextSong" :icon="ArrowRight" circle size="small"></el-button>
             <div>
                 <span>{{ totalTimeFormatted }}</span>
             </div>
@@ -26,6 +26,7 @@ const currentIndex = ref(0)
 const isPlaying = ref(false)
 const audio = ref(null)
 const currentSong = ref({})
+const progressPercentage = ref(0)
 // 当前播放时长（以秒为单位）
 const currentTime = ref(0)
 // 音频总时长（以秒为单位）
@@ -57,9 +58,10 @@ const initMusic = async () => {
   currentSong.value = songList.value[0]
 
 }
-// 进度条相关
-const progressPercentage = ref(0)
-
+/**
+ * @description: 播放/暂停
+ * @return {*}
+ */
 const togglePlay = () => {
   if (isPlaying.value) {
     audio.value.pause()
@@ -69,18 +71,46 @@ const togglePlay = () => {
   isPlaying.value = !isPlaying.value
 }
 
-const prevSong = () => {
+/**
+ * @description: 上一首
+ * @return {*}
+ */
+const changePreSong = () => {
+  audio.value.pause()
+  isPlaying.value = false
   if (currentIndex.value === 0) {
     currentIndex.value = songList.value.length - 1
   } else {
     currentIndex.value--
   }
-  audio.value.src = currentSong.value.src
-  if (isPlaying.value) {
-    audio.value.play()
-  }
+  currentSong.value = songList.value[currentIndex.value]
+  audio.value.src = getFileSrc(currentSong.value.src)
+  setTimeout(() => {
+    togglePlay()
+  }, 500)
 }
-
+/**
+ * @description: 下一首
+ * @return {*}
+ */
+const changeNextSong = () => {
+  audio.value.pause()
+  isPlaying.value = false
+  if (currentIndex.value === songList.value.length - 1) {
+    currentIndex.value = 0
+  } else {
+    currentIndex.value++
+  }
+  currentSong.value = songList.value[currentIndex.value]
+  audio.value.src = getFileSrc(currentSong.value.src)
+  setTimeout(() => {
+    togglePlay()
+  }, 500)
+}
+/**
+ * @description: 播放结束
+ * @return {*}
+ */
 const handleCurrentEnd = () => {
   musicApi.updateMusic({
     id: currentSong.value.id,
@@ -98,7 +128,11 @@ const handleCurrentEnd = () => {
   }
 }
 
-// 更新进度条百分比的函数
+
+/**
+ * @description: 更新进度条百分比
+ * @return {*}
+ */
 const updateProgressPercentage = () => {
   if (audio.value && audio.value.duration) {
     progressPercentage.value = (audio.value.currentTime / audio.value.duration) * 100
@@ -106,6 +140,11 @@ const updateProgressPercentage = () => {
     totalTime.value = audio.value.duration
   }
 }
+/**
+ * @description: 获取文件路径
+ * @param {*} file
+ * @return {*}
+ */
 const getFileSrc = (file) => {
   const url = `http://localhost:1301/music/${file}.mp3`
   return url
@@ -113,7 +152,4 @@ const getFileSrc = (file) => {
 </script>
 
 <style scoped>
-.progress-bar {
-    width: 80%;
-}
 </style>
